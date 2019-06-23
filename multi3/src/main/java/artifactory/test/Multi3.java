a2ef210c11525c70905f252b562eeb0b8c80dd11
gaoxin-jfrogchina <31504568+xingao0803@users.noreply.github.com>
2019-06-23 10:05:10 +0800

    Update Multi3.java

--- multi3/src/main/java/artifactory/test/Multi3.java
+++ multi3/src/main/java/artifactory/test/Multi3.java
@@ -7,5 +7,8 @@ public class Multi3 {
     public static void main(String[] args) {
         new Multi1();
         System.out.println("Hello World!");
+        System.out.println("This is a maven project!");
+        System.out.println("This is a demo for Hygieia!");
+        System.out.println("Hello World!");
     }
 }
